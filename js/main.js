8ac47643e33b813ecb5cf4001dcb3baac8eb1e25
Matt Jones <matthew.jones@ga.co>
2017-04-04 14:45:51 -0700

    Interactions galore!

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
-// $('button').on('mouseenter', function () {
-//     $('#button-hide').addClass('moved');
-// });
+// window.sessionStorage.clear();
 
-// $('button').on('mouseleave', function () {
-//     $('#button-hide').removeClass('moved');   
-// });
+
+var coinLift = "no";
+// var keyFound = "no";
+
+$('.top-button').hide();
+
+
+// When the user finds the key
+$('#key').on('click', function () {
+    // Type code related to event here! :)
+    $('#key').fadeOut();
+    // keyFound = "yes";
+    sessionStorage.setItem('keyFound', "yes");
+});
+
+
+// When the user clicks on the keyhole
+$('#keyhole').on('click', function (event) {
+    event.preventDefault();
+    keyFound = sessionStorage.getItem('keyFound') || "no";
+    if (keyFound === "yes") {
+        $('.hidden-nav').toggleClass('hi-nav');
+    } else {
+        // Show modal window
+    }
+    
+    
+});
 
 // When the user scrolls down,
 $(window).on('scroll', function () {
@@ -12,31 +35,48 @@ $(window).on('scroll', function () {
     var distanceScrolled = $(window).scrollTop();
     console.log('The distance scrolled is: ' + distanceScrolled);
 
-    // if the user has gone 800px down,
     if (distanceScrolled >= 800) {
+        // show "back to top" button
+        $('.top-button').fadeIn();
+    } else {
+        $('.top-button').hide();
+    }
+
+
+    // if the user has gone 800px down,
+    if (distanceScrolled >= 800 && distanceScrolled <= 1200) {
     	// animate the about section
     	$('#headshot').addClass('flipped');
     	$('.headshot-wrapper').addClass('moved');
     	$('.about').addClass('slide');
     	// replace the nav bar with home page navigation
-    	// show "back to top" button
-    // else,
+        $('nav a').addClass('bye-nav')
+
+
+        setTimeout(function() {
+            $('.headshot-wrapper').removeClass('stage-one');
+            $('.headshot-wrapper').addClass('stage-two');
+        }, 4000);
+        setTimeout(function() {
+            $('#key').fadeIn();
+        }, 3200);
     } else {
-    	// move the elements back to their starting place,
+        $('.headshot-wrapper').removeClass('stage-two');
+        $('.headshot-wrapper').addClass('stage-one');
     	$('#headshot').removeClass('flipped');
     	$('.headshot-wrapper').removeClass('moved');
+        $('.headshot-wrapper').removeClass('lifted');
     	$('.about').removeClass('slide');
+        $('#key').fadeOut();
     	// bring back site navigation
     	// hide "back to top" button
     }
 
     // if the user has gone 1600px down,
-    if (distanceScrolled >= 1600) {
+    if (distanceScrolled >= 1600 && distanceScrolled <= 2000) {
     	// animate the mission text,
     	$('.mission').addClass('slide');
-    	$('#headshot').removeClass('flipped');
-    	$('.headshot-wrapper').removeClass('moved');
-    	$('.about').removeClass('slide');
+
     // else,
     } else {
     	// move the text box to its starting place,
@@ -44,18 +84,20 @@ $(window).on('scroll', function () {
     }
 
     // if the user has gone 2400px down,
-    if (distanceScrolled >= 2400) {
+    if (distanceScrolled >= 2400 && distanceScrolled <= 2800) {
     	// animate the ratings section,
     	$('.rating').addClass('slide');
     	$('.mission').removeClass('slide');
+        $('.lock, .key-points').addClass('rated');
     // else,
 	} else {
     	// move the elements back to their starting place,
     	$('.rating').removeClass('slide');
+        $('.lock, .key-points').removeClass('rated');
 	}
 
     // if the user has gone 3200px down,
-    if (distanceScrolled >= 3200) {
+    if (distanceScrolled >= 3200 && distanceScrolled <= 3600) {
     	// animate the disclaimer section,
     	$('.disclaimer').addClass('slide');
     	$('.rating').removeClass('slide');
@@ -67,8 +109,24 @@ $(window).on('scroll', function () {
 
 });
 
+// When the user clicks on the headshot
+$('#headshot').on('click', function () {
+    if (coinLift === "no") {
+        $('.headshot-wrapper').addClass('lifted');
+        coinLift = "yes"
+        console.log(coinLift)
+    } else {
+        $('.headshot-wrapper').removeClass('lifted');
+        coinLift = "no"
+        console.log(coinLift)
+    }
 
-// Do it when someone clicks a nav link
+});
+
+
+
+
+// When the user clicks on the navigation arrows
 $('.arrow-wrapper a').on('click', function(e) {
   // prevent the standard link operation on click
   e.preventDefault();
@@ -84,9 +142,6 @@ $('.arrow-wrapper a').on('click', function(e) {
   }, 600);
 });
 	
-
-
-
 
 
 
